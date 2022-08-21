a07ca189d852517eaf18e7c1e7c2a69bb1f962db
Gabriel Brun <lydstyl@gmail.com>
2022-08-21 22:19:27 +0200

    Build a layout and a basic navbar.

---
+++ components/navbar.tsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return (
+        <ul>
+            <li>
+                <a href="/assets">assets</a>
+            </li>
+            <li>
+                <a href="/categories">categories</a>
+            </li>
+            <li>
+                <a href="/accounts">accounts</a>
+            </li>
+        </ul>
+    )
+}
--- pages/assets/index.tsx
+++ pages/assets/index.tsx
@@ -11,11 +11,6 @@ const Assets: NextPage = () => {
 
             <main>
                 <h1>Assets</h1>
-                <ul>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                </ul>
             </main>
         </div>
     )
