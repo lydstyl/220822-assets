db9c3a54854eef53727381dac9cb7bcabc8793d1
Gabriel Brun <lydstyl@gmail.com>
2022-08-21 22:35:12 +0200

    Display futur features in asset page.

--- pages/assets/index.tsx
+++ pages/assets/index.tsx
@@ -11,6 +11,23 @@ const Assets: NextPage = () => {
 
             <main>
                 <h1>Assets</h1>
+
+                <section>
+                    <h2>Add one asset with form here</h2>
+                </section>
+
+                <section>
+                    <h2>Add multiple with CSV</h2>
+                    <select name="action" id="action">
+                        <option value="replace">replace</option>
+                        <option value="add">add</option>
+                    </select>
+                    <textarea cols="80" rows="10"></textarea>
+                </section>
+
+                <section>
+                    <h2>Show all assets in CSV</h2>
+                </section>
             </main>
         </div>
     )
